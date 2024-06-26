61b5b539ee40f627eb57f34830e1ced89f2ed2be
Safiulla <safiullask75@gmail.com>
2024-06-26 17:22:09 +0530

    add script

---
+++ Js practice/script.js
@@ -0,0 +1,36 @@
+//alert("this is my first code");
+
+// let money = 100;
+// console.log(money);
+
+// let money100 = 45;
+// console.log(money100);
+// money100 = money + 40;
+// console.log(money100);
+
+// console.log(".............................................")
+// let noOfStudent = 40;
+// console.log(noOfStudent)
+// noOfStudent = noOfStudent+5;
+// console.log(noOfStudent)
+
+//use of const.............................................................................;
+// let radius = 4;
+// const pi = 3.14;
+// let area = pi * radius;
+// console.log(area)
+
+// pi = 75;
+// let perimeter= 2*pi*radius;
+// console.log(perimeter);
+
+//string...........................................................................;
+// let str1 = "This is first str";
+// console.log(str1);
+// let str2 = 'This is second str';
+// console.log(str2);
+// noOfStr = 10000
+// let str3 = `str3 use ${noOfStr} `;
+// let str4 = 'str3 use'+ noOfStr;
+// console.log(str3);
+// console.log(str3);
